d07e01855894424dec4d16123de0f6253ead835e
TimJung <TimJung@github.com>
2015-09-08 23:38:21 +0000

    Trip code works, date and time read

--- pi/laserPassageway.c
+++ pi/laserPassageway.c
@@ -1,15 +1,42 @@
+
 #include <wiringPi.h>
 #include <stdio.h>
+#include <time.h>
+#include <unistd.h>
+
 int main (void)
 {
-  wiringPiSetup () ;
-  pinMode (1, INPUT) ;
+  //input is used for measuring what the last input was from the wire
+  int input = 1;
+  //get the system time
+  time_t rawTime;
+  // char hostName[100];
+  // gethostname(&hostName, 100);
+
+  //set up for wiringPi
+  wiringPiSetup();
+  pinMode(1,INPUT);
+  pullUpDnControl(1,PUD_UP);
+
   for (;;)
-  { 
-    	printf("output: ");
-	printf("%i", digitalRead (1));
-	printf("\n");
-	delay (500) ;
+  {
+	//initialize buffer for system time
+	char buffer[26];
+	struct tm* tm_info;
+
+	int current = digitalRead(1);
+	if(input == 1 && current == 0 ){
+		//do time magic
+		time(&rawTime);
+		tm_info = localtime(&rawTime);
+		strftime(buffer, 26, "%Y:%m:%d %H:%M:%S", tm_info);
+	
+		//print or sent to allJoyn router
+		printf("hit: ");
+		printf("%s : %s ", buffer, hostName);
+	}
+	input = current; 
+	delay(250);
   }
-  return 0 ;
+  return 0;
 }
