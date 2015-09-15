956642014ef2e25ec1efe18e14a13e91bd2346fd
TimJung <TimJung@github.com>
2015-09-15 18:06:30 +0000

    Started alarm functionality. Got the double rise/fall working

--- pi/laserPassageway.c
+++ pi/laserPassageway.c
@@ -4,12 +4,11 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/time.h>
 
-//declare your functions
-void pinFall1 (void);
-void pinRise1 (void);
-void pinFall2 (void);
-void pinRise2 (void);
+//function prototype
+void pinHandler1 (void);
+void pinHandler2 (void);
 void setTime (struct tm *);
 
 //pin numbers. 1 is entry. 2 is exit.
@@ -31,41 +30,48 @@ int main (void)
 {
   //set up for wiringPi
   wiringPiSetup();
+
+
   pinMode(BEAM1,INPUT);
   pullUpDnControl(BEAM1,PUD_UP);
   pinMode(BEAM2,INPUT);
   pullUpDnControl(BEAM2, PUD_UP);
-  
+
   wiringPiISR (BEAM1, INT_EDGE_BOTH, pinHandler1);
   wiringPiISR (BEAM2, INT_EDGE_BOTH, pinHandler2);
-  
+
   while (1)
   {
 	wait(NULL);
   }
 }
 
-void pinHandler1 (void){	
+struct timeval tv;
+struct timezone tz;
+
+void pinHandler1 (void){
+gettimeofday (&tv, &tz);
+printf ("%ld %ld\n", tv.tv_sec, tv.tv_usec);
 	if (digitalRead(BEAM1)==1){
 		//if current read is a 1 then it was a fall
 		beam1FallOld = beam1FallLatest;
 		setTime(&beam1FallLatest);
-		
+
 		//output time to test
 		char buffer[26];
-		strftime(buffer, 26, "%Y:%m:%d %H:%M:%S", beam1FallLatest);
+		strftime(buffer, 26, "%Y:%m:%d %H:%M:%S", &beam1FallLatest);
 		printf("Beam 1 fall at: ");
-		printf("%s\n ", buffer);
+		printf("%s\n", buffer);
 	} else {
 		//if current read is a 0 then it was a rise
 		beam1RiseOld = beam1RiseLatest;
 		setTime(&beam1RiseLatest);
-		
+
 		//output time to test
 		char buffer[26];
-		strftime(buffer, 26, "%Y:%m:%d %H:%M:%S", beam1RiseLatest);
+		strftime(buffer, 26, "%Y:%m:%d %H:%M:%S", &beam1RiseLatest);
 		printf("Beam 1 rise at: ");
-		printf("%s\n ", buffer);
+		printf("%s\n", buffer);
 	}
 }
 
@@ -74,22 +80,22 @@ void pinHandler2 (void){
 		//if current read is a 1 then it was a fall
 		beam2FallOld = beam2FallLatest;
 		setTime(&beam2FallLatest);
-		
+
 		//output time to test
 		char buffer[26];
-		strftime(buffer, 26, "%Y:%m:%d %H:%M:%S", beam2FallLatest);
-		printf("Beam 1 fall at: ");
-		printf("%s\n ", buffer);
+		strftime(buffer, 26, "%Y:%m:%d %H:%M:%S", &beam2FallLatest);
+		printf("Beam 2 fall at: ");
+		printf("%s\n", buffer);
 	} else {
 		//if current read is a 0 then it was a rise
 		beam2RiseOld = beam2RiseLatest;
 		setTime(&beam2RiseLatest);
-		
+
 		//output time to test
 		char buffer[26];
-		strftime(buffer, 26, "%Y:%m:%d %H:%M:%S", beam2RiseLatest);
-		printf("Beam 1 rise at: ");
-		printf("%s\n ", buffer);
+		strftime(buffer, 26, "%Y:%m:%d %H:%M:%S", &beam2RiseLatest);
+		printf("Beam 2 rise at: ");
+		printf("%s\n", buffer);
 	}
 }
 
