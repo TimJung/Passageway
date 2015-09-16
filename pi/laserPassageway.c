4351c96f6192fbcc9a5c6e5f044be257a0bab5b0
TimJung <TimJung@github.com>
2015-09-16 21:06:30 +0000

    Store and display time is functional.

--- pi/laserPassageway.c
+++ pi/laserPassageway.c
@@ -12,22 +12,23 @@ int firstRise2 = 1;
 //function prototypes
 void pinHandler1 (void);
 void pinHandler2 (void);
-void setTime (struct tm *);
+struct time {struct timeval tv; struct timezone tz;};
+void setTime (struct time *);
 
 //pin numbers. 1 is entry. 2 is exit.
 const int BEAM1 = 1;
 const int BEAM2 = 4;
 
 //time comparison variables
-struct tm beam1FallOld;
-struct tm beam1RiseOld;
-struct tm beam1FallLatest;
-struct tm beam1RiseLatest;
-struct tm beam2FallOld;
-struct tm beam2RiseOld;
-struct tm beam2RiseLatest;
-struct tm beam2FallLatest;
-struct tm * temp;
+struct time beam1FallOld;
+struct time beam1RiseOld;
+struct time beam1FallLatest;
+struct time beam1RiseLatest;
+struct time beam2FallOld;
+struct time beam2RiseOld;
+struct time beam2RiseLatest;
+struct time beam2FallLatest;
+struct time * temp;
 
 int main (void)
 {
@@ -53,9 +54,6 @@ struct timeval tv;
 struct timezone tz;
 
 void pinHandler1 (void){
-	gettimeofday (&tv, &tz);
-	printf ("%ld %ld\n", tv.tv_sec, tv.tv_usec);
-	
 	if (firstRise1){
 		firstRise1 = 0;
 		return;
@@ -66,21 +64,16 @@ void pinHandler1 (void){
 		setTime(&beam1FallLatest);
 
 		//output time to test
-		char buffer[26];
-		strftime(buffer, 26, "%Y:%m:%d %H:%M:%S", &beam1FallLatest);
 		printf("Beam 1 fall at: ");
-		printf("%s\n", buffer);
+		printf ("%ld %ld\n", beam1FallLatest.tv.tv_sec, beam1FallLatest.tv.tv_usec);		
 	} else {
 		//if current read is a 0 then it was a rise
 			beam1RiseOld = beam1RiseLatest;
 			setTime(&beam1RiseLatest);
 
 			//output time to test
-			char buffer[26];
-			strftime(buffer, 26, "%Y:%m:%d %H:%M:%S", &beam1RiseLatest);
 			printf("Beam 1 rise at: ");
-			printf("%s\n", buffer);
-		
+			printf ("%ld %ld\n", beam1RiseLatest.tv.tv_sec, beam1RiseLatest.tv.tv_usec);		
 	}
 }
 
@@ -97,28 +90,30 @@ void pinHandler2 (void){
 		setTime(&beam2FallLatest);
 
 		//output time to test
-		char buffer[26];
-		strftime(buffer, 26, "%Y:%m:%d %H:%M:%S", &beam2FallLatest);
 		printf("Beam 2 fall at: ");
-		printf("%s\n", buffer);
+		printf ("%ld %ld\n", beam2FallLatest.tv.tv_sec, beam2FallLatest.tv.tv_usec);		
 	} else {
 		//if current read is a 0 then it was a rise
 		beam2RiseOld = beam2RiseLatest;
 		setTime(&beam2RiseLatest);
-
+		
 		//output time to test
-		char buffer[26];
-		strftime(buffer, 26, "%Y:%m:%d %H:%M:%S", &beam2RiseLatest);
 		printf("Beam 2 rise at: ");
-		printf("%s\n", buffer);
+		printf ("%ld %ld\n", beam2RiseLatest.tv.tv_sec, beam2RiseLatest.tv.tv_usec);		
 	}
 }
 
-void setTime(struct tm * ptr){
-	time_t currentTime;
- 	currentTime = timegm(ptr);
-	temp = gmtime(&currentTime);
-	ptr = temp;
+void setTime(struct time * ptr){
+	struct time t1 = *ptr;
+	struct timeval tv1 = t1.tv;
+	struct timezone tz1 = t1.tz;
+	gettimeofday (&tv1, &tz1);
+//	gettimeofday (ptr.tv, ptr.tz);
+//	printf ("%ld %ld\n", tv.tv_sec, tv.tv_usec);
+//	printf ("%ld %ld\n", tv1.tv_sec, tv1.tv_usec);
+	t1.tv = tv1;
+	t1.tz = tz1;
+	*ptr = t1;
 }
 
 //Pi Machine Name/ID
